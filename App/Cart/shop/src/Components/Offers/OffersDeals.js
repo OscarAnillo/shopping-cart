b4c5dd9a43e6f87c4a8b73e4cfcb79735d72f877
Oscar Anillo <you@example.com>
2021-03-16 16:22:32 -0500

    add gsap animation

--- App/Cart/shop/src/Components/Offers/OffersDeals.js
+++ App/Cart/shop/src/Components/Offers/OffersDeals.js
@@ -1,3 +1,4 @@
+import {useRef, useEffect} from 'react';
 /* Components */
 import offers from '../Data/OffersData';
 
@@ -7,6 +8,9 @@ import {makeStyles} from '@material-ui/core/styles';
 
 /* React-router */
 import { Link } from 'react-router-dom';
+
+/* gsap */
+import gsap from 'gsap';
 
 const useStyles = makeStyles(theme => ({
     divBg: {
@@ -37,16 +41,23 @@ const useStyles = makeStyles(theme => ({
 
 export default function OffersDeals(){
     const classes = useStyles();
+    const typographyRef = useRef(null);
+    const divRef = useRef(null);
     const {data} = offers;
     
+    useEffect(() => {
+        gsap.fromTo(typographyRef.current, {duration: 1, scale: 4, ease: 'back'}, {duration: 1,scale: 1, ease: 'back'})
+        gsap.fromTo('.divMap', {y: 200, opacity: 0, duration: 2}, {y: 0, opacity: 1, duration: 2})
+    }, [])
+
     return(
         <div className={classes.divBg}>
             <div className={classes.divText}>
-                <Typography className={classes.typographyStyle}>DEALS!</Typography>
+                <Typography className={classes.typographyStyle} ref={typographyRef}>DEALS!</Typography>
             </div>
             <section>
                 {data.map(x => (
-                    <div>
+                    <div key={x.id} ref={divRef} className="divMap">
                         <Link to={`/offers/${x.name}`} className={classes.linkStyle}>
                             <Card className={classes.cardStyle}>
                                 <CardHeader title={x.name}/>
