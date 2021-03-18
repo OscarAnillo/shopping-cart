0d06df0c3f630e7cff43bffc0adf72586a3b846f
OscarAnillo <oscarani14@gmail.com>
2021-03-17 20:39:30 -0500

    add adidas models to adidas files

--- App/Cart/shop/src/Components/Brands/ShoeBrands/Adidas.js
+++ App/Cart/shop/src/Components/Brands/ShoeBrands/Adidas.js
@@ -1,16 +1,19 @@
 import {useRef, useEffect} from 'react';
 
 /* Material-ui */
-import {AppBar, Tab, Toolbar, IconButton, Badge, Typography, Tabs} from '@material-ui/core';
+import {AppBar, Tab, Toolbar, IconButton, Badge, Typography, Tabs, Card, CardHeader, CardMedia} from '@material-ui/core';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import {makeStyles} from '@material-ui/core/styles';
+
+/* component */
+import adidasModels from '../../Models/ShoeBrandModels/AdidasModels'
 
 /* Gsap */
 import gsap from 'gsap/gsap-core';
 
 const useStyles = makeStyles(theme => ({
     divBg: {
-        height: '100vh',
+        minHeight: '100vh',
         backgroundImage: 'url(https://assets.adidas.com/images/w_600,f_auto,q_auto/ff2e419f1eda4ebab23faad6009a3a9e_9366/Superstar_Shoes_White_EG4958.jpg)',
         backgroundPosition:'center',
         backgroundSize: 'cover',
@@ -26,17 +29,36 @@ const useStyles = makeStyles(theme => ({
         },
         
     },
+    typographyStyle: {
+        fontSize: '2.5rem'
+    },
+    divMap: {
+        border: '1px solid #eee',
+        width: '100%',
+
+    },
+    cardStyle: {
+        margin: '1em'
+
+    },
+    cardImage: {
+        height: 230,
+        [theme.breakpoints.up('sm')] : {
+            height: 330
+        }
+    }
     
 }))
 
-export default function Adidas(){
+export default function Adidas(props){
     const classes = useStyles();
+    const {product} = adidasModels;
     const bgRef = useRef(null);
     
-
     useEffect(() => {
         gsap.fromTo(bgRef.current, {opacity: 0, duration: 2}, {opacity: 1, duration: 2})
     }, [])
+
     return(
         <div className={classes.divBg} ref={bgRef}>
             <section >
@@ -47,11 +69,24 @@ export default function Adidas(){
                             <Tab label="prices"/>
                         </Tabs>
                         <div>
-                        <Badge><ShoppingCartIcon /></Badge>
+                            <Badge color="secondary"><ShoppingCartIcon /></Badge>
                         </div>
                     </Toolbar>
                 </AppBar>
             </section>
+            <div>
+                <Typography className={classes.typographyStyle  }>Our Models</Typography>
+                <div>
+                    {product.map(x => (
+                        <div className={classes.divMap}>
+                            <Card className={classes.cardStyle}>
+                                <CardHeader title={x.name} subheader={x.discount}/>
+                                <CardMedia image={x.image} className={classes.cardImage}/>
+                            </Card>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
---
+++ App/Cart/shop/src/Components/Models/ShoeBrandModels/AdidasModels.js
@@ -0,0 +1,12 @@
+const adidasModels = {
+    product : [
+        {id:'1', name:'Ultra Boost', discount:'30%', image:'https://footwearnews.com/wp-content/uploads/2019/11/adidas-ultraboost-20.jpg'},
+        {id:'2', name:'Adidas NMD', discount:'25%', image:'https://footwearnews.com/wp-content/uploads/2020/01/nmd_r1_shoes_black_fv3645_01_standard-e1577981113856.jpg?resize=1024,586'},
+        {id:'3', name:'Pharrell Hu', discount:'20%', image:'https://footwearnews.com/wp-content/uploads/2019/08/pw_hu_nmd_prd_white_eg7737_01_standard-e1566228621903.jpg?resize=1024,577'},
+        {id:'4', name:'Futurecraft ', discount:'25%', image:'https://footwearnews.com/wp-content/uploads/2018/01/adidas-futurecraft-4d2.jpg'},
+        {id:'5', name:'Yeezy Boost ', discount:'20%', image:'https://footwearnews.com/wp-content/uploads/2019/05/f36980_01_standard-e1559234261768.jpg?resize=1024,578'},
+        {id:'6', name:'Super-Star', discount:'30%', image:'https://footwearnews.com/wp-content/uploads/2017/09/adidas-superstar-sneakers.jpg'},
+    ]
+}
+
+export default adidasModels;
